bce9819811756a6f0218cfed6e5ea8fd5d5458b6
Huang Yangkai <addlovemax@gmail.com>
2018-11-16 11:43:04 +0100

    Tymeleaf gemacht!

--- src/main/java/de/hhu/propra/db/KundeController.java
+++ src/main/java/de/hhu/propra/db/KundeController.java
@@ -1,10 +1,13 @@
 package de.hhu.propra.db;
 
 import de.hhu.propra.db.data.KundeRepository;
+import de.hhu.propra.db.entities.Kunde;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class KundeController{
@@ -13,6 +16,8 @@ public class KundeController{
 
     @GetMapping("/")
     public String index(Model model) {
+        List<Kunde> all = kunden.findAll();
+        model.addAttribute("kunden", all);
         return "index";
     }
 }
